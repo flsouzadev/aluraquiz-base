73baaa5c9b102adbe1d07787e49b517426c058a5
flsouzadev <48827921+flsouzadev@users.noreply.github.com>
2021-02-09 14:33:34 -0300

    3 aula, inclusão de informações

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -1,18 +1,17 @@
-/* eslint-disable react/jsx-props-no-spreading */
-import PropTypes from 'prop-types';
 import React from 'react';
+import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
 const InputBase = styled.input`
-width: 100%;
-padding: 15px;
-font-size: 14px;
-border: 1px solid ${({ theme }) => theme.colors.primary};
-color: ${({ theme }) => theme.colors.contrastText};
-background-color: ${({ theme }) => theme.colors.mainBg};
-border-radius: ${({ theme }) => theme.borderRadius};
-outline: 0;
-margin-bottom: 25px;
+  width: 100%;
+  padding: 15px;
+  font-size: 14px;
+  border: 1px solid ${({ theme }) => theme.colors.primary};
+  color: ${({ theme }) => theme.colors.contrastText};
+  background-color: ${({ theme }) => theme.colors.mainBg};
+  border-radius: ${({ theme }) => theme.borderRadius};
+  outline: 0;
+  margin-bottom: 25px;
 `;
 
 export default function Input({ onChange, placeholder, ...props }) {
@@ -21,6 +20,7 @@ export default function Input({ onChange, placeholder, ...props }) {
       <InputBase
         placeholder={placeholder}
         onChange={onChange}
+        // eslint-disable-next-line react/jsx-props-no-spreading
         {...props}
       />
     </div>
@@ -28,13 +28,12 @@ export default function Input({ onChange, placeholder, ...props }) {
 }
 
 Input.defaultProps = {
-    value: '',
-    
+  value: '',
 };
 
 Input.propTypes = {
-    onChange: PropTypes.func.isRequired,
+  onChange: PropTypes.func.isRequired,
   placeholder: PropTypes.string.isRequired,
-    name: PropTypes.string.isRequired,
-  value: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  value: PropTypes.string,
 };
